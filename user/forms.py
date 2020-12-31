4ca0744be014aa1556f5ea8eec27873a64bc6897
Lucky2307 <alana.luki@gmail.com>
2021-01-01 02:17:03 +0700

    Implemented login.html

--- user/forms.py
+++ user/forms.py
@@ -37,8 +37,7 @@ class PasswordResetForm(forms.Form):
 class AuthenticationForm(AuthenticationForm):
     error_messages = {
         'invalid_login': _(
-            "Please enter a correct %(username)s and password. Note that both "
-            "fields may be case-sensitive."
+            "Incorrect username and password."
         ),
         'inactive': _("You must activate your email before logging in."),
     }
